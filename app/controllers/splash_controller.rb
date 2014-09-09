5a9fd18082501eefdade7350246c34d03eac0458
ajordan510 <alex.jordan.j@gmail.com>
2014-09-08 19:15:12 -0700

    functionality for admin

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,21 +1,31 @@
 class SplashController < ApplicationController
   def index
-    @title = 'Oddie'
+    @title = 'SpectaFresh'
     #@js_file_to_include = ['jquery.final-countdown.js', 'jquery.final-countdown.min.js', 'kinetic.js']
   end
 
   def create
     email = params[:splash_email_form][:splash_email]
+    role = params[:role]
     if email == nil
       redirect_to :controller => 'splash', :action => 'index' #check if email is valid in more robust way - @ sign etc.
     else
       if SplashUser.find_by_email(email) == nil
-        @new_splash_user = SplashUser.create(:email => email)
+        @new_splash_user = SplashUser.create(:email => email, :role => role)
       else
         flash[:email_taken] = "Email has already been issued. thanks for being so enthusiastic!"
       end
-      redirect_to :controller => 'splash', :action => 'confirmation' #give the option to send requests to other people
+        flash[:confirm] = "Thank you for you interest.  We will keep you up to date on the SpectaFresh release."
+      redirect_to :controller => 'splash', :action => 'index' #give the option to send requests to other people
     end
   end    
+
+  def admin
+    @title = 'SpectaFresh - Admin'
+    @users = SplashUser.find(:all)
+    @performers = SplashUser.find(:all, :conditions => {:role => "performer"})
+    @audience = SplashUser.find(:all, :conditions => {:role => "audience"})
+    @both = SplashUser.find(:all, :conditions => {:role => "both"})
+  end
 
 end
--- db/migrate/20140807003250_create_splash_users.rb
+++ db/migrate/20140807003250_create_splash_users.rb
@@ -2,6 +2,7 @@ class CreateSplashUsers < ActiveRecord::Migration
   def change
     create_table :splash_users do |t|
       t.text :email
+      t.text :role
       t.timestamps
     end
   end
