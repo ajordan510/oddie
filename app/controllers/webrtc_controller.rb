e1dde960d5a49debbeacbbd1614450308a572b8b
ajordan510 <alex.jordan.j@gmail.com>
2014-09-24 21:10:09 -0700

    started performer page

--- app/controllers/webrtc_controller.rb
+++ app/controllers/webrtc_controller.rb
@@ -1,5 +1,12 @@
 class WebrtcController < ApplicationController
   def test
+  	@title='SpectaFresh'
   	render :layout => 'webrtc'
+  end
+
+  def performer
+  	@title='SpectaFresh'
+  	render :layout => 'webrtc'
+
   end
 end
